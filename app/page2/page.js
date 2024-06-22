818bf6adc9f893de5872c1c2cd59ce9a683bb344
Zach Blume <zachblume@gmail.com>
2024-06-22 14:35:16 -0400

    initial commit

---
+++ app/page2/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default async function Page2() {
+  return (
+    <>
+      Page 2
+      <br/>
+      <Link href="/">Go to Home Page</Link>
+    </>
+  );
+}
